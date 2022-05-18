b13af97eba31e1cdf8ea3ea033f1a1244b07b475
f-kurdan <fkurdan@gmail.com>
2022-05-18 16:15:55 +0300

    Add Meta Props to ViewModel

--- Blog/ViewModels/PostViewModel.cs
+++ Blog/ViewModels/PostViewModel.cs
@@ -6,7 +6,12 @@
 
 		public string Title { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
-        public string CurrentImage { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+		public string Tags { get; set; } = string.Empty;
+		public string Category { get; set; } = string.Empty;
+
+		public string CurrentImage { get; set; } = string.Empty;
         public IFormFile Image { get; set; } = null;
 	}
 }
