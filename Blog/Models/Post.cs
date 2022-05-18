7b49020fde81a7bf456bdabfb893f36a1dc46e89
f-kurdan <fkurdan@gmail.com>
2022-05-18 16:15:01 +0300

    Add Meta Props to Post Model

---
+++ Blog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Models
+{
+    public class Post
+    {
+        public int? ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        public string Tags { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
