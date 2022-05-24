123511c51016e2316bda88ee557a7566b3b0f427
f-kurdan <fkurdan@gmail.com>
2022-05-25 00:46:02 +0300

    Update Edit method

--- Blog/Controllers/PanelController.cs
+++ Blog/Controllers/PanelController.cs
@@ -65,7 +65,11 @@ namespace Blog.Controllers
             if (vm.Image == null)
                 post.Image = vm.CurrentImage;
             else
+            {
+                if (!string.IsNullOrEmpty(vm.CurrentImage))
+                    _fileManager.RemoveImage(vm.CurrentImage);
                post.Image = await _fileManager.SaveImage(vm.Image);
+            }
 
             if (post.ID == 0)
                 _repo.AddPost(post);
